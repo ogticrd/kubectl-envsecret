b50fec4b624e9e0b0a4437310bc51de37d3387b2
Jorge Massih <jorgmassih@gmail.com>
2024-06-10 11:57:03 -0400

    ✅ add test for `k8sapi` package

---
+++ internal/k8sapi/k8s_test.go
@@ -0,0 +1,49 @@
+package k8sapi_test
+
+import (
+	"testing"
+
+	"github.com/ogticrd/kubectl-envsecret/internal/k8sapi"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestK8sCreateSecret(t *testing.T) {
+	fakeClient := fake.NewSimpleClientset()
+
+	k := k8sapi.NewK8sClient(fakeClient, "test")
+
+	t.Run("test CreateSecret returns expected results", func(t *testing.T) {
+		err := k.CreateSecret("test", mockSecretData())
+		assert.Nil(t, err)
+	})
+	t.Run("test CreateSecret fails with alreadyExists", func(t *testing.T) {
+		err := k.CreateSecret("test", mockSecretData())
+		assert.NotNil(t, err)
+		assert.True(t, kerr.IsAlreadyExists(err))
+	})
+}
+
+func mockSecretData() map[string]string {
+	secret := make(map[string]string)
+	secret["foo"] = `line1
+  line2
+  line3
+  line4`
+	secret["bar"] = "line"
+	return secret
+}
+
+func mockSecret(secret map[string]string) *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "test",
+			Namespace:       "test",
+			OwnerReferences: []metav1.OwnerReference{},
+		},
+		StringData: secret,
+	}
+}
